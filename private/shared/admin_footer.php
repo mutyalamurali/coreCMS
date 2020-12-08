b64fe9d9ad63654fb21a41c48c5e7e2c494e0861
Murali Mutyala <murali@itsclue.com>
2020-12-08 09:44:29 +0530

    [Refactor] - basic index file refactoring

---
+++ private/shared/admin_footer.php
@@ -0,0 +1,9 @@
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!-- Bootstrap Javascript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js" integrity="sha512-8qmis31OQi6hIRgvkht0s6mCOittjMa9GMqtK9hes5iEQBQE/Ca6yGE5FsW36vyipGoWQswBj/QBm2JR086Rkw==" crossorigin="anonymous"></script>
+<!-- Custom javascript -->
+<script src="<?php echo url('js/custom.js'); ?>"></script>
+</body>
+
+</html>
