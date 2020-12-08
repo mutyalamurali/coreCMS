653147ff5dc8a23825caa1f937efbae274115aa6
Murali Mutyala <murali@itsclue.com>
2020-12-08 20:56:06 +0530

    [DELETE] deletes query test

--- html/admin/index.php
+++ html/admin/index.php
@@ -3,10 +3,5 @@
 <!-- Admin Header -->
 <?php include_once SHARED_PATH . DS . 'admin_header.php'; ?>
 
-<?php $var1 = "<script>
-    alert('hello')
-</script>"; ?>
-<?php var_dump(findByQuery('SELECT * FROM subjects where id >= 1 ')); ?>
-
 <!-- Admin Footer -->
 <?php include_once SHARED_PATH . DS . 'admin_footer.php'; ?>
