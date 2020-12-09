5415a73386861f8168719b644d9d102ea9163fa2
Mutyala Murali <murali@itsclue.com>
2020-12-09 21:50:44 +0530

    Revert "Development"

--- html/admin/index.php
+++ html/admin/index.php
@@ -3,17 +3,9 @@
 <!-- Admin Header -->
 <?php include_once SHARED_PATH . DS . 'admin_header.php'; ?>
 
+<?php var_dump(); ?>
 <!-- Admin Nav -->
 <?php include_once SHARED_PATH . DS . 'admin_nav.php'; ?>
-
-<main class="container">
-    <h2>Main Menu</h2>
-
-    <ul>
-        <li><a href="<?php echo url('admin/subjects/index.php'); ?>">Menu Names</a></li>
-        <li><a href="<?php echo url('admin/pages/index.php'); ?>">Pages</a></li>
-    </ul>
-</main>
 
 <!-- Javascript scripts file -->
 <?php include_once SHARED_PATH . DS . 'js_scripts.php'; ?>
