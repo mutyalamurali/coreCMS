c46678d512e3c47135ee19ce8ec1d3fb196bd594
Murali Mutyala <murali@itsclue.com>
2020-12-09 21:23:40 +0530

    [ADD] - create new subjects and pages index file

---
+++ html/admin/pages/index.php
@@ -0,0 +1,20 @@
+<?php require_once '../../../private/init.php'; ?>
+
+<!-- Admin Header -->
+<?php include_once SHARED_PATH . DS . 'admin_header.php'; ?>
+
+
+<!-- Admin Nav -->
+<?php include_once SHARED_PATH . DS . 'admin_nav.php'; ?>
+
+<main class="container">
+    <h2>Pages</h2>
+</main>
+
+
+<!-- Javascript scripts file -->
+<?php include_once SHARED_PATH . DS . 'js_scripts.php'; ?>
+
+
+<!-- Admin Footer -->
+<?php include_once SHARED_PATH . DS . 'admin_footer.php'; ?>
