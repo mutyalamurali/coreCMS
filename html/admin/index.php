d69bfcdf71a370c1b6bcb579be699f134a0a52da
Murali Mutyala <murali@itsclue.com>
2020-12-09 21:18:00 +0530

    [ADD] - creates main menu for admin

--- html/admin/index.php
+++ html/admin/index.php
@@ -7,6 +7,15 @@
 <!-- Admin Nav -->
 <?php include_once SHARED_PATH . DS . 'admin_nav.php'; ?>
 
+<main class="container">
+    <h2>Main Menu</h2>
+
+    <ul>
+        <li><a href="<?php echo url('admin/subjects/index.php'); ?>">Menu Names</a></li>
+        <li><a href="<?php echo url('admin/pages/index.php'); ?>">Pages</a></li>
+    </ul>
+</main>
+
 <!-- Javascript scripts file -->
 <?php include_once SHARED_PATH . DS . 'js_scripts.php'; ?>
 
