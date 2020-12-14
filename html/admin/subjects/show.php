cee91c36de1ace77fa3422e807e75fe3d0cabc1c
Murali Mutyala <murali@itsclue.com>
2020-12-14 18:19:13 +0530

    [UPDATE] - show subject details

--- html/admin/subjects/show.php
+++ html/admin/subjects/show.php
@@ -14,8 +14,18 @@
     if ($id == 0) {
         $id = 1;
     }
-    echo $id;
+
+    $subject_result = findById('subjects', $id);
     ?>
+
+    <a href="<?php echo url('admin/subjects/index.php'); ?>">&laquo; Back</a>
+
+    <h2>Subject: <?php echo $subject_result['menu_name']; ?></h2>
+
+    <b>ID:</b> <?php echo $subject_result['id']; ?><br>
+    <b>Menu Name:</b> <?php echo $subject_result['menu_name']; ?><br>
+    <b>Position:</b> <?php echo $subject_result['position']; ?><br>
+    <b>Visible: </b><?php echo $subject_result['visible'] == 1 ? 'True' : 'False'; ?>
 </main>
 
 <!-- Javascript scripts file -->
