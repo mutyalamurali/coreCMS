b843a816c1ce5f03589421dc0995d914d0f0b898
Murali Mutyala <murali@itsclue.com>
2021-04-07 19:29:56 +0530

    [ADD] users list page

---
+++ html/admin/users.php
@@ -0,0 +1,19 @@
+<?php require_once '../../private/init.php'; ?>
+
+<!-- Admin Header -->
+<?php include_once SHARED_PATH . DS . 'admin_header.php'; ?>
+
+<!-- Admin Nav -->
+<?php include_once SHARED_PATH . DS . 'admin_nav.php'; ?>
+
+<div class="container">
+    <h1>Users</h1>
+
+
+</div>
+
+<!-- Javascript scripts file -->
+<?php include_once SHARED_PATH . DS . 'js_scripts.php'; ?>
+
+<!-- Admin Footer -->
+<?php include_once SHARED_PATH . DS . 'admin_footer.php'; ?>
